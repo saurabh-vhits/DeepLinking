befb35ec02dc716955acd06b43b93e1fa6037bf9
saurabh-vhits <saurabhsoni.vhits@gmail.com>
2024-05-29 19:05:34 +0530

    Update name

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
-import CustomStatusBar from './common/CustomStatusBar/CustomStatusBar';
+import CustomStatusBar from './common/customStatusBar/CustomStatusBar';
 import Header from './common/header/Header';
 import RNText from './common/rnText/RNText';
 import RnTouchableOpacity from './common/rnTouchableOpacity/RNTouchableOpacity';
