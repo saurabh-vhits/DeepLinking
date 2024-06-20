04f83e3481f2def58fd6346143a05ab4cdfef0e7
saurabh-soni-dev <saurabh.soni6408@gmail.com>
2024-06-20 08:49:27 +0530

    Add video

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -29,3 +29,63 @@ const Home: FC = () => {
 };
 
 export default Home;
+
+
+// const onUploadExerciseVideo = useCallback(
+//   async (item: ExercisesProps, index: number) => {
+//     const exerciseVideo = await ImageCropPicker.openPicker({
+//       mediaType: 'video',
+//       compressVideoPreset: 'HighestQuality',
+//     });
+//     if (exerciseVideo && item && index) {
+//       const {
+//         title,
+//         defaultParametersSets,
+//         defaultParametersReps,
+//         tags,
+//         equipment,
+//         mainMuscle,
+//         secondaryMuscle,
+//         trainerInstructions,
+//       } = item;
+//       try {
+//         const formData = new FormData();
+//         formData.append(param.default.videoExercise, {
+//           name: 'video.mp4',
+//           uri: exerciseVideo.path,
+//           type: 'video/mp4',
+//         });
+//         formData.append(
+//           param.default.createNewExerciseData,
+//           JSON.stringify({
+//             title: title,
+//             defaultParametersSets: defaultParametersSets,
+//             defaultParametersReps: defaultParametersReps,
+//             type: '',
+//             tags: tags,
+//             equipment: equipment,
+//             mainMuscle: mainMuscle,
+//             secondaryMuscle: secondaryMuscle,
+//             trainerInstructions: trainerInstructions,
+//           }),
+//         );
+//         const {data} = await axiosInstance.post(
+//           constant.createExercise,
+//           formData,
+//           {
+//             headers: {
+//               Authorization: `Bearer ${token}`,
+//               'Content-Type': 'multipart/form-data',
+//             },
+//           },
+//         );
+//         if (data) {
+//           Toast(data?.message);
+//         }
+//       } catch (error: any) {
+//         Toast(error?.response?.data?.message);
+//       }
+//     }
+//   },
+//   [bodyWeightOnlyState],
+// );
